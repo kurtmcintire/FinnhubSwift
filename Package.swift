6ec09dd6c40c88a74ca152e3ab5edfef1c1c4003
Kurt McIntire <kurt.mcintire@gmail.com>
2020-10-13 15:32:24 -0500

    Setup test suite

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "FinnhubSwift",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v13), .macOS(.v10_15),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -22,7 +22,15 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(name: "FinnhubSwift",
-                path: "Sources"),
+        .target(
+            name: "FinnhubSwift",
+            dependencies: [],
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "FinnhubSwiftTests",
+            dependencies: ["FinnhubSwift"],
+            path: "Tests"
+        ),
     ]
 )
